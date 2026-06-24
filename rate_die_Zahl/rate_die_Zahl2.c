88f0108eec1c417352582c8c8db5e483765f1ede
rprussne rprussne <rprussne@c2r8s3.wels.42.school>
2026-06-24 10:47:44 +0200

    write finish

--- rate_die_Zahl/rate_die_Zahl2.c
+++ rate_die_Zahl/rate_die_Zahl2.c
@@ -6,13 +6,14 @@
 /*   By: rprussne <rprussne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/11 10:42:41 by rprussne          #+#    #+#             */
-/*   Updated: 2026/06/21 07:02:01 by rprussne         ###   ########.fr       */
+/*   Updated: 2026/06/24 10:44:02 by rprussne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <unistd.h>
 
 int	main(void)
 {
@@ -21,8 +22,8 @@ int	main(void)
 	int	leben;
 
 	srand(time(NULL));
-	printf("willkommen zu Rate die Zahl 2!\n");
-	printf("Rate eine Zahl zwischen 1 und 10: ");
+	write(1, "Willkommen zu Rate die Zahl 2!\n", 31);
+	write(1, "Rate eine Zahl zwischen 1 und 10: ", 34);
 
 	char	antwort;
 	antwort = 'j';
@@ -34,25 +35,43 @@ int	main(void)
 	
 		while(leben > 0)
 		{
-			printf("Tipp: ");
+			write(1, "Tipp: ", 6);
 			scanf("%d", &tipp);
 			
 			if (tipp == geheimzahl)
 			{
-				printf("Gewonnen!\n");
+				write(1, "Gewonnen!\n", 10);
 				break;
 			}
 			leben--;
 			if (leben == 0)
 			{
-				printf("Verloren! Die Zahl war %d\n", geheimzahl);
+				write(1, "Verloren!\n", 10);
+				write(1, "Die Zahl war: ", 14);
+				if (geheimzahl == 10)
+				{
+					write(1, "10\n", 3);
+				}
+				else
+				{
+					char	zahl;
+
+					zahl = geheimzahl + '0';
+					write(1, &zahl, 1);
+					write(1, "\n", 1);
+				}
 			}
 			else
 			{
-				printf("Falsch! Leben: %d\n", leben);
+				char	zahl;
+
+				zahl = leben + '0';
+				write(1, "Falsch Leben: ", 15);
+				write(1, &zahl, 1);
+				write(1, "\n", 1);
 			}
 		}
-		printf("Nochmal spielen? (j/n): ");
+		write(1, "Nochmal spielen? (j/n): ", 24);
 		scanf(" %c", &antwort);
 	}
 }
